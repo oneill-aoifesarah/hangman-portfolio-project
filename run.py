64208a99c09b0779136f2ae98e33b6b341570fdb
oneill-aoifesarah <>
2023-11-30 03:46:07 +0000

    feat:define and display hangman stages function

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from words import word_list_easy, word_list_medium, word_list_hard
 
 def get_word(difficulty):
     """
-    Get a random word based on the difficulty level.
+    Get a random word based on the difficulty level
     """
     if difficulty == 'easy':
         return random.choice(word_list_easy).upper()
@@ -17,3 +17,68 @@ def get_word(difficulty):
         return random.choice(word_list_hard).upper()
     else:
         raise ValueError("Invalid difficulty level")
+
+
+def display_hangman(attempts):
+    """
+    Display the hangman in six stages based on the number of incorrect attempts
+    """
+    hangman_stages = [
+        """
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |   / \\
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |   /
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|
+           |
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |    |
+           |
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |
+           |
+           |
+        """,
+        """
+           ------
+           |    |
+           |
+           |
+           |
+           |
+        """
+    ]
+    return hangman_stages[attempts]
