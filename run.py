37357250c63439b9a81397ed7a0d3d55f74dccb0
oneill-aoifesarah <>
2023-11-30 03:25:24 +0000

    feat:add get_word function for words with account for difficulty

--- run.py
+++ run.py
@@ -3,3 +3,15 @@ Randomize and import word list
 """
 import random
 from words import word_list_easy, word_list_medium, word_list_hard
+
+
+def get_word(difficulty):
+    """
+    Get a random word based on the difficulty level.
+    """
+    if difficulty == 'easy':
+        return random.choice(word_list_easy).upper()
+    elif difficulty == 'medium':
+        return random.choice(word_list_medium).upper()
+    elif difficulty == 'hard':
+        return random.choice(word_list_hard).upper()
