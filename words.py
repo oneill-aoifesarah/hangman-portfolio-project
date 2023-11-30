19ca1c1af4ea887a741e54cd788153092aa77d82
oneill-aoifesarah <>
2023-11-30 03:05:37 +0000

    feat:update words.py file

---
+++ words.py
@@ -0,0 +1,29 @@
+# Easy Words
+word_list_easy = [
+    "dog", "cat", "sun", "hat", "run",
+    "cup", "bed", "top", "pen", "key",
+    "bug", "jam", "lip", "fan", "map",
+    "mud", "hut", "fox", "jug", "nut",
+    "hop", "pig", "log", "dot", "fix",
+    "bat", "bus", "leg", "hut", "pan"
+]
+
+# Medium Words
+word_list_medium = [
+    "tiger", "river", "apple", "dance", "chair",
+    "cloud", "guitar", "happy", "movie", "ocean",
+    "paper", "radio", "smile", "beach", "dream",
+    "horse", "money", "watch", "candy", "lemon",
+    "robot", "piano", "tiger", "river", "apple",
+    "dance", "chair", "cloud", "guitar", "happy"
+]
+
+# Hard Words
+word_list_hard = [
+    "symphony", "quantum", "zephyr", "epiphany", "kaleidoscope",
+    "exquisite", "phosphorescent", "byzantine", "pizzazz", "quixotic",
+    "labyrinth", "serendipity", "nebulous", "cacophony", "clandestine",
+    "esoteric", "formidable", "idiosyncrasy", "juxtapose", "labyrinthine",
+    "mellifluous", "obfuscate", "paucity", "querulous", "quintessential",
+    "resplendent", "surreptitious", "ubiquitous", "verisimilitude", "zenith"
+]
